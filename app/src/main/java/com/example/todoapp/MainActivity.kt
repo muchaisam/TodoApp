2ac5dc6799a76a1d39b01c8fa68e3fc6493dffd3
muchaisam <samson.muchai@strathmore.edu>
2021-12-02 12:27:29 +0300

    adapter+rview

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    val listofTasks = mutableListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        //when the button is clicked
+//        findViewById<Button>(R.id.button).setOnClickListener{
+//            //executed when a user clicks the button
+//
+//        }
+
+        listofTasks.add("Do laundry")
+        listofTasks.add("Wash dishes")
+
+        // Lookup the recyclerview in activity layout
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        // Initialize contacts
+        val adapter = TaskItemAdapter(listofTasks)
+        // Attach the adapter to the recyclerview to populate items
+        recyclerView.adapter = adapter
+        // Set layout manager to position the items
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
