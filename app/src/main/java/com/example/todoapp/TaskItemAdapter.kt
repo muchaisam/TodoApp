7381df874b28f18cc5a280f84f98cea5cef83b6e
muchaisam <samson.muchai@strathmore.edu>
2021-12-02 12:26:39 +0300

    listofitems

---
+++ app/src/main/java/com/example/todoapp/TaskItemAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.todoapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TaskItemAdapter(val listOfItems : List<String>) : RecyclerView.Adapter<TaskItemAdapter.ViewHolder>() {
+
+    // Usually involves inflating a layout from XML and returning the holder
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        // Inflate the custom layout
+        val contactView = inflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+        // Return a new holder instance
+        return ViewHolder(contactView)
+    }
+
+    // Involves populating data into the item through holder
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // Get the data model based on position
+        val item  = listOfItems.get(position)
+        // Set item views based on your views and data model
+        holder.textView.text = item
+
+    }
+
+    override fun getItemCount(): Int {
+        return listOfItems.size
+    }
+
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        //store references to our views
+        val textView:TextView
+
+        init {
+            textView = itemView.findViewById(android.R.id.text1)
+        }
+    }
+
+}
+
